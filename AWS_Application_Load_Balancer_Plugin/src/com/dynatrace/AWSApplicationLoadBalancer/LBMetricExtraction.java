a0b8822ef49377226ae9f7ac83f477de6c8b9a80
Adam Gardner <adam.gardner@dynatrace.com>
2017-04-05 11:27:53 +0100

    Fix for issue hliang12/#1
    Fix for https://github.com/hliang12/AWSApplicationLoadBalancerPlugin/issues/1

--- AWS_Application_Load_Balancer_Plugin/src/com/dynatrace/AWSApplicationLoadBalancer/LBMetricExtraction.java
+++ AWS_Application_Load_Balancer_Plugin/src/com/dynatrace/AWSApplicationLoadBalancer/LBMetricExtraction.java
@@ -88,11 +88,19 @@ public class LBMetricExtraction {
         String nameSpace = "AWS/ApplicationELB";
         String name = "LoadBalancer";
 
-        		GetMetricStatisticsRequest request1 = new GetMetricStatisticsRequest()
+	Dimension oDimension = getDimension(client, name, lbName);
+        
+        if (oDimension == null)
+        {
+        	log.severe("Unable to find requested load balancer. Please check monitor configuration.");
+        }
+		
+        GetMetricStatisticsRequest request1 = new GetMetricStatisticsRequest()
                 .withStartTime(startTime)
                 .withNamespace(nameSpace)
                 .withPeriod(time)
-                .withDimensions(new Dimension().withName(name).withValue(lbName))
+                //.withDimensions(new Dimension().withName(name).withValue(lbName))
+		.withDimensions(oDimension)
                 .withMetricName(metricName)
                 .withStatistics(aggregationList)
                 .withEndTime(endTime);
@@ -100,5 +108,36 @@ public class LBMetricExtraction {
         return client.getMetricStatistics(request1);
 
     }
+	
+	/*
+	 * We've provided the LB name eg. "load1" which forms part of the ARN.
+	 * We need to search for the dimension that contains the LB name so we can dynamically extract the entire ARN
+	 * and then use that to build the GetMetricStatisticsRequest object.
+	 * 
+	 * This is needed in case we destroy an instance of "load1" then create a new LB with the same name.
+	 * We want monitoring to continue and using this method, it will.
+	 */
+	private Dimension getDimension(AmazonCloudWatch client, String dimensionName, String lbName)
+	{
+		List<Metric> oMetricList = new ArrayList<Metric>();
+		try
+		{
+			oMetricList = client.listMetrics().getMetrics();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			return null;
+		}
+		
+		for (Metric oMetric : oMetricList)
+		{
+			for (Dimension oDim : oMetric.getDimensions())
+			{
+				if (oDim.getName().contains(dimensionName) && oDim.getValue().contains(lbName))return oDim; 
+			}
+		}
+		return null;
+	}
 
 }
